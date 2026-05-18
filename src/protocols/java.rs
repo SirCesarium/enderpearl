99d19f02bcd8d9a6480a52939205738074273365
SirCesarium <cesarium3600@gmail.com>
2026-05-18 18:18:08 -0400

    refactor: simplify packet identification logic in MinecraftJava protocol

--- src/protocols/java.rs
+++ src/protocols/java.rs
@@ -11,15 +11,7 @@ define_protocol!(
 
             match data.get(1) {
                 Some(&0x00) if packet_len >= 5 && data.len() > packet_len => {
-                    let state_index = packet_len;
-
-                    match data.get(state_index) {
-                        Some(&1 | &2) => {
-                            let protocol_ver = data[2];
-                            protocol_ver > 0 && protocol_ver < 0x80
-                        }
-                        _ => false,
-                    }
+                    matches!(data.get(packet_len), Some(&1 | &2))
                 }
                 _ => false,
             }
